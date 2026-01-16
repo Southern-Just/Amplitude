1751443ba92007bedfb7a3042cf9f49f61f047db
Southern Just <karuguezekiel@gmail.com>
2026-01-16 19:14:53 +0300

    start 000

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import DataTable from "@/components/DataTable";
 import Image from "next/image";
 const page = () => {
   return (
     <main className="main-container">
       <section className="home-grid">
         <div id="coin-overview">
-          <div>
+          <div className="header">
             <Image
-            width={90} height={90}
+              width={56}
+              height={56}
               src="https://cdn.pixabay.com/photo/2018/01/18/07/31/bitcoin-3089728_960_720.jpg"
               alt="bitcoin"
+              className="rounded-full"
             />
+            <div className="info">
+              <p>Bitcoin / BTC</p>
+              <h1>$89,113.00</h1>
+            </div>
           </div>
         </div>
         <p>Trending coins</p>
+        <DataTable columns={[{header:"title"}]} />
       </section>
       <section className="w-full mt-7 space-y-4">
-        <p>Catgories</p>
+        <p>Categories</p>
       </section>
     </main>
   );
---
+++ components/DataTable.tsx
@@ -0,0 +1,47 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+
+const DataTable  =<T,> ({
+  columns,
+  data,
+  rowKey,
+  tableClassName,
+  headerClassName,
+  headerRowClassName,
+  headerCellClassName,
+  bodyRowClassName,
+  bodyCellClassName,
+}: DataTableProps <T>) => {
+  return (
+    <Table className={cn("custom-scrollbar", tableClassName)}>
+      <TableHeader className={headerClassName}>
+        <TableRow className={cn('hover:bg-transparent!',
+            headerRowClassName)}>
+            {columns.map((column, i) => (
+              <TableHead key={i} className={cn('bg-dark-400 text-purple-100 py-4 ')}>
+                {column.header}
+            </TableHead>
+            ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
+
+export default DataTable;
